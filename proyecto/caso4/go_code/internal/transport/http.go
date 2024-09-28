525e269c827255cee72a5b707d85a6bceb0c7a8d
Pamreth29 <pamreth29@gmail.com>
2024-09-27 22:03:30 -0600

    primer endpoint listo

--- proyecto/caso4/go_code/internal/transport/http.go
+++ proyecto/caso4/go_code/internal/transport/http.go
@@ -7,8 +7,10 @@ import (
     "fmt"
     "net/http"
     "Caso4/internal/endpoint"
+    "Caso4/pkg/db"
 )
 
+// DecodeGetDataRequest decodifica la solicitud para obtener datos
 func DecodeGetDataRequest(_ context.Context, r *http.Request) (interface{}, error) {
     var request endpoint.GetDataRequest
     if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
@@ -17,10 +19,26 @@ func DecodeGetDataRequest(_ context.Context, r *http.Request) (interface{}, erro
     return request, nil
 }
 
+// EncodeResponse codifica la respuesta en formato JSON
 func EncodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
     return json.NewEncoder(w).Encode(response)
 }
 
+// HomeHandler es el handler para la ruta principal
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "¡Hola! Bienvenido al microservicio.")
+}
+
+// get35PercentHandler maneja la solicitud para obtener el 35% de los registros
+func get35PercentHandler(pgDB *db.PostgresDB) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        registros, err := pgDB.Get35PercentRecords()  // Cambia a Get35PercentRecords
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(registros)
+    }
 }

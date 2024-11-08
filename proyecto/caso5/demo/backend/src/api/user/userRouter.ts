2784402e18a6b0de0e5d9be3a7abeb37aabc3994
Pamreth29 <pamreth29@gmail.com>
2024-11-08 15:22:58 -0600

    version 1 para lo de servicios adicionales

--- proyecto/caso5/demo/backend/src/api/user/userRouter.ts
+++ proyecto/caso5/demo/backend/src/api/user/userRouter.ts
@@ -10,5 +10,6 @@ userRouter.get("/favorites/:userID", (req, res) => userController.getFavoritesBy
 userRouter.post("/", (req, res) => userController.createUser(req, res));
 userRouter.post("/favorites", (req, res) => userController.addFavorite(req, res));
 userRouter.put("/favorites/:userID/:cuidadorID", (req, res) => userController.deleteFavorite(req, res));
+userRouter.post("/search-additional-services", (req, res) => userController.searchAdditionalServices(req, res));
 
 export { userRouter }; 
